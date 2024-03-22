7359fbec7f28fc9984923e7fa13597c69c3a310f
DEED1T <hamdi.lazaar@proton.me>
2024-03-22 10:08:38 +0100

    Add get touching and  coords on touch

--- app/src/main/java/com/as/challenge/GameView.java
+++ app/src/main/java/com/as/challenge/GameView.java
@@ -4,19 +4,24 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 
 import com.as.challenge.utility.Constants;
 
-public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+public class GameView extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
+
     private GameActivity _activity;
     private GameThread _thread;
 
     private int _x = 0;
     private int _y = 0;
+    private boolean _isTouching= false;
 
     public GameView(Context context_) {
         super(context_);
@@ -26,6 +31,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         _thread = new GameThread(getHolder(), this);
         _y = _activity.getSharedPreferences().getInt(Constants.KEY_VALUE_Y, 1);
 
+        setOnTouchListener(this);
         setFocusable(true);
     }
 
@@ -45,26 +51,44 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
             try {
                 _thread.setRunning(false);
                 _thread.join();
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             retry = false;
         }
     }
-
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
         if (canvas != null) {
             canvas.drawColor(Color.WHITE);
 
-            Paint paint = new Paint();
-            paint.setColor(Color.rgb(250, 0 , 0));
-            canvas.drawRect(_x, 100 * _y, _x +100, 200, paint);
         }
     }
-    public void update() {
-        _x = (_x + 1) % 300;
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                _isTouching = true;
+                _x = (int) event.getX();
+                _y = (int) event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                _isTouching = false;
+                break;
+        }
+        return true;
     }
+
+    public void update() {
+        if (_isTouching){
+            Log.d("TOUCH", "X -> "+ _x);
+            Log.d("TOUCH", "Y -> "+_y);
+            Log.d("TOUCH", "Touch -> "+ _isTouching);
+        }
+    }
+
 
 }
